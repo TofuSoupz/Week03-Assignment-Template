292e53dea2b204d05108776028173d1690600ea1
Harry Gossage <harry.gossage@hotmail.com>
2024-08-30 14:59:29 -0400

    Update inner.py

--- 02-inner/inner.py
+++ 02-inner/inner.py
@@ -1,5 +1,12 @@
 # Write here you solution for the 02-inner exercise
 
 # Prompt the user to write something in all caps, e.g., HELLO, WORLD!
+txt = "Hello World!"
+x = txt.upper()
+print(x)
 
 # Print it in lower case, e.g., hello, world!
+
+ftf = "Hello World!"
+f = ftf.lower()
+print(f)
